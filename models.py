1b041f68e70b4dbd6aba1ca9f24abf93eee609b5
Abdullah Arshad <abdullaharshad287@gmail.com>
2025-05-22 14:19:44 +0500

    updated tables to store user's data and record.

---
+++ models.py
@@ -0,0 +1,52 @@
+from flask_bcrypt import Bcrypt
+from flask_login import UserMixin
+from sqlalchemy.orm import DeclarativeBase, relationship, sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+
+bcrypt = Bcrypt()
+
+DATABASE_URL = 'sqlite:///users.db'
+engine = create_engine(DATABASE_URL, echo=True)
+Session = sessionmaker(bind=engine)
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base, UserMixin):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(150), nullable=False)
+    email = Column(String(150), unique=True, nullable=False)
+    password = Column(String(256), nullable=False)
+
+    data = relationship('Data', back_populates='user', cascade='all, delete-orphan')
+
+    def set_password(self, raw_password):
+        self.password = bcrypt.generate_password_hash(raw_password).decode('utf-8')
+
+    def check_password(self, raw_password):
+        return bcrypt.check_password_hash(self.password, raw_password)
+
+    def __repr__(self):
+        return f"<User {self.username}, {self.email}>"
+
+class Data(Base):
+    __tablename__ = 'users_data'
+
+    id = Column(Integer, primary_key=True)
+    record = Column(String, nullable=False)
+
+    user = relationship('User', back_populates='data')
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Data for User ID {self.user}>"
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+
+    session = Session()
+    session.query(Data).delete()
+    session.query(User).delete()
+    session.commit()
