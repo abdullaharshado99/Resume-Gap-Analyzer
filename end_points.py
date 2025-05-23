daed6acf28295cfc4201b563417c5272576424e3
Abdullah Arshad <abdullaharshad287@gmail.com>
2025-05-23 19:20:39 +0500

    created endpoints for mock_interview

--- end_points.py
+++ end_points.py
@@ -1,6 +1,8 @@
 import os
-from models import engine, Session, User, bcrypt, Base
-from flask import Flask, render_template, request, redirect, url_for, flash
+# from crypt import methods
+from models import engine, Session, User, bcrypt, Base, Data
+from pipeline import extract_text_from_doc, generate_gap_summary, mock_interview
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
@@ -25,7 +27,14 @@ def load_user(user_id):
 def index():
     return render_template("index.html", user=current_user)
 
+@app.route('/chatbot', methods=['POST', 'GET'])
+@login_required
+def chatbot():
+    return render_template('mock_interview.html')
+
+
 @app.route('/process', methods=['POST'])
+@login_required
 def process():
     resume_file = request.files['resume']
     job_description = request.form['job_description']
@@ -36,9 +45,45 @@ def process():
         filename = resume_file.filename
         filepath = os.path.join(uploaded_folder, filename)
         resume_file.save(filepath)
-        return f"Received file: {filename}<br>Job Description: {job_description[:200]}..."
 
-    return "Error: No file uploaded"
+        parsed_data = extract_text_from_doc(filepath)
+        resume_text = parsed_data[0].page_content
+        summary = generate_gap_summary(resume_text, job_description)
+
+        session = Session()
+        new_data = Data(
+            summary=summary,
+            resume_data=resume_text,
+            job_description=job_description,
+            user_id=current_user.id
+        )
+        session.add(new_data)
+        session.commit()
+
+        return render_template("response.html", data=summary)
+
+    return "Error: No file uploaded", 404
+
+
+@app.route('/mock_interview', methods=['POST', 'GET'])
+@login_required
+def interview():
+
+    try:
+        data = request.get_json(force=True)
+        query = data.get("query", "").strip()
+
+        if not query:
+            return jsonify({"response": "Query is missing"}), 400
+
+        response = mock_interview(query)
+        print(f'The {response=}')
+        return jsonify({"response": response})
+
+    except Exception as e:
+        return jsonify({"response": f"Error: {str(e)}"}), 400
+
+
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
